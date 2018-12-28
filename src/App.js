02e7273c9fc65851f3af8b655ac8d67670348792
Sang Bui <sangbui51@yahoo.com>
2018-12-28 14:45:45 -0800

    Step 0: configure redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 
+import { createStore, combineReducers  } from 'redux';
+import { Provider } from 'react-redux';
+
+import searchData from './reducer';
+
+
+const allReducers = combineReducers({
+    searchData
+});
+
+const store = createStore(
+    allReducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+
 class App extends Component {
-  render() {
-    return (
-      <div>
-        hello world!
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Provider store={store} >
+                <main>
+                    <input placeholder="search"/>
+                    <ul>
+                        <li>some data</li>
+                        <li>some data</li>
+                        <li>some data</li>
+                        <li>some data</li>
+                        <li>some data</li>
+                        <li>some data</li>
+                        <li>some data</li>
+                    </ul>
+                </main>
+            </Provider>
+        );
+    }
 }
 
 export default App;
